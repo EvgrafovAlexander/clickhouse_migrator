de5c9b2ecb61d5163c06d0b78ed51f22bd95b965
EvgrafovAlexander <nemesis92@me.com>
2023-05-26 13:27:51 +0500

    feat(migrator) add pre-commit

--- tests/test_migrator.py
+++ tests/test_migrator.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 # thirdparty
 from clickhouse_driver import Client
+
 from click_migrator.migrator import Migrator
 
 
@@ -53,24 +54,9 @@ class TestMigrator(unittest.TestCase):
         """
 
         cls.expected_table_data = [
-            {
-                "id": 1,
-                "value": 2.3,
-                "bool_value": 1,
-                "str_value": "comment1"
-            },
-            {
-                "id": 2,
-                "value": 4.5,
-                "bool_value": 0,
-                "str_value": "comment2"
-            },
-            {
-                "id": 3,
-                "value": 6.7,
-                "bool_value": 1,
-                "str_value": "comment3"
-            }
+            {"id": 1, "value": 2.3, "bool_value": 1, "str_value": "comment1"},
+            {"id": 2, "value": 4.5, "bool_value": 0, "str_value": "comment2"},
+            {"id": 3, "value": 6.7, "bool_value": 1, "str_value": "comment3"},
         ]
 
     @classmethod
@@ -101,20 +87,20 @@ class TestMigrator(unittest.TestCase):
                 "version": 1,
                 "file_name": "001.create.default.table1.sql",
                 "checksum": "d18349d55813148857afb191d85b7350",
-                "success": True
+                "success": True,
             },
             {
                 "version": 2,
                 "file_name": "002.insert.default.table1.sql",
                 "checksum": "dec826b82a1960265a8091eaa4448069",
-                "success": True
+                "success": True,
             },
             {
                 "version": 3,
                 "file_name": "003.create.default.table2.sql",
                 "checksum": "90cfb131c0a9b98bf8b597e323eacd85",
-                "success": True
-            }
+                "success": True,
+            },
         ]
         self.assertListEqual(sorted_history, expected_history)
         self.assertListEqual(sorted_table1_data, self.expected_table_data)
@@ -135,14 +121,14 @@ class TestMigrator(unittest.TestCase):
                 "version": 1,
                 "file_name": "001.create_and_insert.default.table1.sql",
                 "checksum": "2ce3555d8210ba604b85a41bf9446d5d",
-                "success": True
+                "success": True,
             },
             {
                 "version": 2,
                 "file_name": "002.create.default.table2.sql",
                 "checksum": "90cfb131c0a9b98bf8b597e323eacd85",
-                "success": True
-            }
+                "success": True,
+            },
         ]
         self.assertListEqual(sorted_history, expected_history)
         self.assertListEqual(sorted_table1_data, self.expected_table_data)
